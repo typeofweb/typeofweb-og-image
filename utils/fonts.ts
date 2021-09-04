1a34ae346a21b1b7a8b2e638f668bb3f0c08470f
Michal Miszczyszyn <michal@mmiszy.pl>
2021-09-04 02:05:13 +0200

    Fix font path

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,21 +1,33 @@
-import Fs from 'fs';
-import Path from 'path';
+import Fs from "fs";
+import Path from "path";
 
-const mono = Fs.readFileSync(Path.join(process.cwd(), 'public', 'fonts', 'firamono-regular-typeofweb.woff2')).toString('base64');
-const rglr = Fs.readFileSync(Path.join(process.cwd(), 'public', 'fonts', 'firasans-regular-typeofweb.woff2')).toString('base64');
-const bold = Fs.readFileSync(Path.join(process.cwd(), 'public', 'fonts', 'firasans-semibold-typeofweb.woff2')).toString('base64');
+const path = Path.join(
+  process.cwd(),
+  process.env.VERCEL ? "" : "public",
+  "fonts"
+);
+
+const mono = Fs.readFileSync(
+  Path.join(path, "firamono-regular-typeofweb.woff2")
+).toString("base64");
+const rglr = Fs.readFileSync(
+  Path.join(path, "firasans-regular-typeofweb.woff2")
+).toString("base64");
+const bold = Fs.readFileSync(
+  Path.join(path, "firasans-semibold-typeofweb.woff2")
+).toString("base64");
 
 export const fontCss = `
 @font-face {
   font-family: 'Fira Sans';
-  font-style:  normal;
+  font-style: normal;
   font-weight: normal;
   src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
 }
 
 @font-face {
   font-family: 'Fira Sans';
-  font-style:  normal;
+  font-style: normal;
   font-weight: bold;
   src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
 }
